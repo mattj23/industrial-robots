8f9d9e182dc53048ce7fa842d9190befed879152
Matt Jarvis <mattj23@gmail.com>
2023-05-27 12:36:15 -0400

    more public exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod poses;
 pub mod robot;
 
-pub use k::nalgebra::{Isometry3, Matrix4, Vector3};
+pub use k::nalgebra::{Isometry3, Matrix4, Vector3, try_convert, UnitQuaternion, Translation3};
 pub use poses::XyzWpr;
+pub use robot::FanucLrMate200id;
